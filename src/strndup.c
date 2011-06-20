ba854d81c3740ca5af6cdc6fa4dbc21d9fd7de5d
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2011-06-20 15:17:17 +0000

    bugfix from klibc 1.5.23
    git-svn-id: https://svn.kapsi.fi/jpa/paatti/baselibc@936 e3a754e5-d11d-0410-8d38-ebb782a927b9

--- src/strndup.c
+++ src/strndup.c
@@ -10,8 +10,10 @@ char *strndup(const char *s, size_t n)
 	int l = n > strlen(s) ? strlen(s) + 1 : n + 1;
 	char *d = malloc(l);
 
-	if (d)
-		memcpy(d, s, l);
+	if (!d)
+		return NULL;
+	
+	memcpy(d, s, l);
 	d[n] = '\0';
 	return d;
 }
